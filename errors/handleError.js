56afdb83f3249bf169c0dd7cfd8b0e0115e7b333
Alex Garshin <gooddiz1@yandex.ru>
2023-07-20 17:20:19 +0300

    fix

--- errors/handleError.js
+++ errors/handleError.js
@@ -1,18 +1,17 @@
 class CustomError extends Error {
   constructor(statusCode, message) {
-    super();
+    super(message);
     this.statusCode = statusCode;
-    this.message = message;
   }
 }
 
 const handleError = (err, res) => {
   const { statusCode, message } = err;
-  res.status(statusCode).json(statusCode.toString().startsWith('5') ? {
+  res.status(statusCode).json({
     status: 'error',
     statusCode,
     message,
-  } : new CustomError(statusCode, message));
+  });
 };
 
 module.exports = {
