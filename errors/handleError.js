2f341bdf03bc97f29b0889eec30b664632c26562
Alex Garshin <gooddiz1@yandex.ru>
2023-07-20 20:05:25 +0300

    add fix

--- errors/handleError.js
+++ errors/handleError.js
@@ -5,21 +5,21 @@ class CustomError extends Error {
   }
 }
 
-const handleError = (err, res) => {
-  const { statusCode, message } = err;
-  res.status(statusCode).json({
-    status: 'error',
-    statusCode,
-    message,
-  });
-};
-
 // const handleError = (err, res) => {
-//   if (err instanceof CustomError) {
-//     return res.status(err.statusCode).send({ message: err.message });
-//   }
-//   return res.status(500).send({ message: 'Ошибка сервера' });
+//   const { statusCode, message } = err;
+//   res.status(statusCode).json({
+//     status: 'error',
+//     statusCode,
+//     message,
+//   });
 // };
+
+const handleError = (err, res) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ message: err.message });
+  }
+  return res.status(500).send({ message: 'Ошибка сервера' });
+};
 
 module.exports = {
   CustomError,
